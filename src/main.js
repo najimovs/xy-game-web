65640264cb030eb00d16998c50e224230fae5985
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-08-22 15:49:39 +0500

    mana commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+const buttons = document.querySelectorAll( "button" )
 
+buttons.forEach( button => {
+
+	button.onclick = () => {
+
+		button.textContent = [ "X", "Y", "Z" ][ ( Math.random() * 3 ) | 0 ]
+	}
+} )
